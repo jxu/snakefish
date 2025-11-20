1e34f0a43183e4df5e21689f89d17e710c2af214
jxu <7989982+jxu@users.noreply.github.com>
2025-11-19 22:45:00 -0500

    Add move promotion letter to UCI string

--- move.py
+++ move.py
@@ -1,27 +1,34 @@
 from board import *
 
+PROMO_MAP = {
+    KNIGHT: 'n',
+    BISHOP: 'b',
+    ROOK: 'r',
+    QUEEN: 'q',
+}
+
 class Move:
     """Encode move with from square, to square, and flags.
 
     https://www.chessprogramming.org/Encoding_Moves
     """
-    def __init__(self, from_ind, to_ind, flags=0):
+    def __init__(self, from_ind, to_ind, capture=False, promotion=EMPTY):
         assert sq_valid(from_ind)
         assert sq_valid(to_ind)
         assert from_ind != to_ind  # null move
 
         self.from_ = from_ind
         self.to = to_ind
-        self.flags = flags  # TODO
-
+        self.capture = capture
+        self.promotion = promotion
 
     def __str__(self):
         """Return pure algebraic coordinate notation, like h7h8q"""
 
-        # TODO: implement promotions
-        if self.flags:
-            raise NotImplementedError
+        s = sq_to_coord(self.from_) + sq_to_coord(self.to)
+        if self.promotion:
+            s += PROMO_MAP[self.promotion]
 
-        return sq_to_coord(self.from_) + sq_to_coord(self.to)
+        return s
 
 
--- test_move.py
+++ test_move.py
@@ -2,4 +2,5 @@ from move import *
 
 
 def test_move_str():
-    assert str(Move(sq_from_coord("a1"), sq_from_coord("c2"))) == "a1c2"
+    assert str(Move(SQ("a1"), SQ("c2"))) == "a1c2"
+    assert str(Move(SQ("e7"), SQ("e8"), promotion=QUEEN)) == "e7e8q"
